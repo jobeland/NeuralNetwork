c98ec5a5663dc1632dd22ce85174a727ab88699f
jordan.beland@gmail.com <jordan.beland@gmail.com>
2015-06-29 23:51:03 -0700

    Add neuron class

---
+++ NeuralNetwork/NeuralNetwork/Neuron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    [Serializable]
+    public class Neuron
+    {
+
+        protected double output { get; set; }
+        protected double input { get; set; }
+
+        public Neuron()
+        {
+            output = 0;
+            input = 0;
+        }
+
+        protected double calculateActivationFunction()
+        {
+            return input;
+        }
+
+        protected void fire()
+        {
+            this.output = calculateActivationFunction();
+            this.input = 0;
+        }
+
+    }
+}
