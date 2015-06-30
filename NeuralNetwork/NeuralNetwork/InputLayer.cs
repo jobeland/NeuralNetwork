77f08b0802a2d519afcec480e162c783ee0fc027
jordan.beland@gmail.com <jordan.beland@gmail.com>
2015-06-30 00:03:47 -0700

    add input layer

---
+++ NeuralNetwork/NeuralNetwork/InputLayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+
+    [Serializable]
+    public class InputLayer
+    {
+
+        public List<Neuron> NeuronsInLayer { get; set; }
+
+        public InputLayer(int numberOfNeuronsInLayer)
+        {
+            NeuronsInLayer = new List<Neuron>();
+            for (int i = 0; i < numberOfNeuronsInLayer; i++)
+            {
+                Neuron n = new Neuron();
+                NeuronsInLayer.add(n);
+            }
+        }
+        public void SetInputs(double[] inputs)
+        {
+            int length = inputs.length;
+            if (length != NeuronsInLayer.Count)
+            {
+                Console.WriteLine("Warning: length of inputs does not match input layer");
+                if (length > NeuronsInLayer.Count)
+                {
+                    length = NeuronsInLayer.Count;
+                }
+            }
+            for (int i = 0; i < length; i++)
+            {
+                NeuronsInLayer[i].Input = inputs[i];
+            }
+        }
+
+        public void FireAll()
+        {
+            foreach (Neuron n in NeuronsInLayer)
+            {
+                n.fire();
+            }
+        }
+    }
+
+}
