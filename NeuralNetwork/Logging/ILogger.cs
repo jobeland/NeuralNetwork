eb17dd5a679508cdd13ccde569cb6d00cc56bb88
jobeland <jordan.beland@gmail.com>
2015-07-29 21:31:51 -0700

    Add log4net logger

---
+++ NeuralNetwork/Logging/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    interface ILogger
+    {
+        public void Log(LogLevel level, string message);
+    }
+}
---
+++ NeuralNetwork/Logging/Log4NetLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class Log4NetLogger : ILogger
+    {
+        private readonly ILog _loggerImpl;
+
+        public void Log(LogLevel level, string message)
+        {
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    _loggerImpl.Debug(message);
+                    break;
+                case LogLevel.Info:
+                    _loggerImpl.Info(message);
+                    break;
+                case LogLevel.Warning:
+                    _loggerImpl.Warn(message);
+                    break;
+                case LogLevel.Error:
+                    _loggerImpl.Error(message);
+                    break;
+                case LogLevel.Fatal:
+                    _loggerImpl.Fatal(message);
+                    break;
+            }
+        }
+    }
+}
