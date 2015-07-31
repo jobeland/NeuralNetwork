9108fe6964d51e8921250e22228f396eb308a6b0
jobeland <jordan.beland@gmail.com>
2015-07-30 23:40:22 -0700

    Add identity function

---
+++ NeuralNetwork/NeuralNetwork/ActivationFunctions/IdentityActivationFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork.ActivationFunctions
+{
+    public class IdentityActivationFunction : IActivationFunction
+    {
+        public double Calculate(double sumOfInputsAndBias)
+        {
+            return sumOfInputsAndBias;
+        }
+    }
+}
