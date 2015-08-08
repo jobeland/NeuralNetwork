691af50b14cfed1cb710fdfc63e61e5da8addbe3
jobeland <jordan.beland@gmail.com>
2015-08-07 23:11:42 -0700

    add soma factory

---
+++ NeuralNetwork/NeuralNetwork/Factories/SomaFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork.Factories
+{
+    public class SomaFactory
+    {
+        private ISummationFunction _summationFunction;
+
+        private SomaFactory(ISummationFunction summationFunction)
+        {
+            _summationFunction = summationFunction;
+        }
+
+        public static SomaFactory GetInstance(ISummationFunction summationFunction)
+        {
+            return new SomaFactory(summationFunction);
+        }
+
+        public ISoma Create(IList<Synapse> dendrites)
+        {
+            return new Soma(dendrites, _summationFunction);
+        }
+    }
+}
