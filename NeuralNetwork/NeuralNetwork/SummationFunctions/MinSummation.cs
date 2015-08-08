e5896c3b6b4f92addd153b09bc435f26be319d19
jobeland <jordan.beland@gmail.com>
2015-08-07 23:32:21 -0700

    add min summation function

---
+++ NeuralNetwork/NeuralNetwork/SummationFunctions/MinSummation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork.SummationFunctions
+{
+    public class MinSummation : ISummationFunction
+    {
+        public double CalculateSummation(IList<Synapse> dendrites)
+        {
+            double min = double.MaxValue;
+            foreach (Synapse synapse in dendrites)
+            {
+                if (synapse.Value < min)
+                {
+                    min = synapse.Value;
+                }
+            }
+            return min;
+        }
+    }
+}
