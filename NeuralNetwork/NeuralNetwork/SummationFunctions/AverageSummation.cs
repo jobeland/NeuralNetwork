765fc57744a3c6ff672a72057edbfd2ba053d715
jobeland <jordan.beland@gmail.com>
2015-08-07 23:33:38 -0700

    add average summation

---
+++ NeuralNetwork/NeuralNetwork/SummationFunctions/AverageSummation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork.SummationFunctions
+{
+    public class AverageSummation : ISummationFunction
+    {
+        public double CalculateSummation(IList<Synapse> dendrites)
+        {
+            double average = 0.0;
+            if(dendrites.Count == 0)
+            {
+                return 0.0;
+            }
+            foreach (Synapse synapse in dendrites)
+            {
+                average += synapse.Value
+            }
+            return average / dendrites.Count;
+        }
+    }
+}
