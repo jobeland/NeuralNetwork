c8424e4866450782f98f151eedeec415377ffcb6
jobeland <jordan.beland@gmail.com>
2015-08-07 23:30:50 -0700

    add max summation function

---
+++ NeuralNetwork/NeuralNetwork/SummationFunctions/MaxSummation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialNeuralNetwork.SummationFunctions
+{
+    public class MaxSummation : ISummationFunction
+    {
+        public double CalculateSummation(IList<Synapse> dendrites)
+        {
+            double max = double.MinValue;
+            foreach (Synapse synapse in dendrites)
+            {
+                if (synapse.Value > max)
+                {
+                    max = synapse.Value;
+                }
+            }
+            return max;
+        }
+    }
+}
