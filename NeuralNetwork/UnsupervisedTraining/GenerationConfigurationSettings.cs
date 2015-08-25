8949d0e201bf8e7288cc05aea425d5b2112f1776
jobeland <jordan.beland@gmail.com>
2015-08-24 22:52:49 -0700

    add untracked file

---
+++ NeuralNetwork/UnsupervisedTraining/GenerationConfigurationSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsupervisedTraining
+{
+    public class GenerationConfigurationSettings
+    {
+        public bool UseMultithreading { get; set; }
+        public int GenerationPopulation { get; set; }
+    }
+}
