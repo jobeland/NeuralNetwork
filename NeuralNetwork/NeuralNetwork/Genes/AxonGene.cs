a5b1b9b7022c45047cdd039ea6d302696e40e1a7
jobeland <jordan.beland@gmail.com>
2016-07-28 23:18:32 -0700

    remove unused directives

--- NeuralNetwork/NeuralNetwork/Genes/AxonGene.cs
+++ NeuralNetwork/NeuralNetwork/Genes/AxonGene.cs
@@ -1,9 +1,6 @@
-﻿using ArtificialNeuralNetwork.ActivationFunctions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArtificialNeuralNetwork.Genes
 {
