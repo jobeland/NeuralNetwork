d28d22be333d86604e7b392dc153686b85b69334
jobeland <jordan.beland@gmail.com>
2016-08-07 19:48:00 -0700

    update network factory interface

--- NeuralNetwork/NeuralNetwork/Factories/INeuralNetworkFactory.cs
+++ NeuralNetwork/NeuralNetwork/Factories/INeuralNetworkFactory.cs
@@ -10,6 +10,7 @@ namespace ArtificialNeuralNetwork.Factories
     public interface INeuralNetworkFactory
     {
         INeuralNetwork Create(int numInputs, int numOutputs, int numHiddenLayers, int numHiddenPerLayer);
+        INeuralNetwork Create(int numInputs, int numOutputs, IList<int> hiddenLayerSpecs);
         INeuralNetwork Create(NeuralNetworkGene genes);
     }
 }
