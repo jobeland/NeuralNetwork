9de94bdfec421e224b03b034b095d83c09bd3062
jobeland <jordan.beland@gmail.com>
2017-06-29 23:18:36 -0700

    add ReLU and Leaky ReLU

---
+++ NeuralNetwork/NeuralNetwork/ActivationFunctions/LeakyRectifiedLinearActivationFunction.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtificialNeuralNetwork.ActivationFunctions
+{
+    public class LeakyRectifiedLinearActivationFunction : IActivationFunction
+    {
+        public double CalculateActivation(double signal)
+        {
+            return signal > 0.0 ? signal : 0.01*signal;
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/ActivationFunctions/RectifiedLinearActivationFunction.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtificialNeuralNetwork.ActivationFunctions
+{
+    public class RectifiedLinearActivationFunction : IActivationFunction
+    {
+        public double CalculateActivation(double signal)
+        {
+            return signal > 0 ? signal : 0.0;
+        }
+    }
+}
